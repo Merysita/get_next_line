71498c4772a38db5513c17b29e13a3f614c9ee84
Merysita <miaury@DESKTOP-0VG2I8S.localdomain>
2026-01-26 21:50:17 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -1,75 +1,75 @@
 #include "get_next_line.h"
 
-char	*custom_realloc(char *tmp, char buff[BUFF_SIZE])
+//Tenemos que redimensionar stash hasta que contenga un \n o \0
+char	*resize(char *stash, char buff[BUFF_SIZE])
 {
-	char	*keep_tmp;
-	size_t	len_tmp;
+	char	*keep_stash;
+	size_t	len_stash;
 	size_t	len_buff;
 
-	len_tmp = ft_strlen(tmp);
-	keep_tmp = (char *) malloc(len_tmp * sizeof(char));
-	if (!keep_tmp)
-		return (NULL);
-	keep_tmp = ft_memcpy(keep_tmp, tmp, len_tmp);
-	free(tmp);
-	len_buff = ft_strlen(buff);
-	tmp = (char *) malloc((len_tmp + len_buff) * sizeof(char));
-	if (!tmp)
-		return (NULL);
-	tmp = ft_memcpy(tmp, keep_tmp, len_tmp);
-	tmp = ft_memcpy(&tmp[len_tmp], buff, len_buff);
-	return (tmp);
+	while (ft_strchr(stash, '\n') == NULL || ft_strchr(stash, '\0') == NULL)
+	{
+		len_stash = ft_strlen(stash);
+		keep_stash = (char *) malloc(len_stash * sizeof(char));
+		if (!keep_stash)
+			return (free(stash), NULL);
+		keep_stash = ft_memcpy(keep_stash, stash, len_stash);
+		free(stash);
+		len_buff = ft_strlen(buff);
+		stash = (char *) malloc((len_stash + len_buff) * sizeof(char));
+		if (!stash)
+			return (NULL);
+		stash = ft_memcpy(stash, keep_stash, len_stash);
+		stash = ft_memcpy(&stash[len_stash], buff, len_buff);
+	}
+	return (stash);
 }
 
-char	*free_tmp(char *tmp, char *line)
+char	*free_stash(char *stash, char *line)
 {
-	char	*keep_tmp;
-	size_t	len_keep_tmp;
+	char	*keep_stash;
+	size_t	len_keep_stash;
 	size_t	len_line;
 
-	len_keep_tmp = ft_strlen(ft_strchr(tmp, '\n'));
-	keep_tmp = (char *) malloc(len_keep_tmp * sizeof(char));
-	if (!keep_tmp)
+	len_keep_stash = ft_strlen(ft_strchr(stash, '\n'));
+	keep_stash = (char *) malloc(len_keep_stash * sizeof(char));
+	if (!keep_stash)
 		return (NULL);
-	keep_tmp = ft_memcpy(keep_tmp, ft_strchr(tmp, '\n'), len_keep_tmp);
-	len_line = ft_strlen(tmp) - len_keep_tmp;
+	keep_stash = ft_memcpy(keep_stash, ft_strchr(stash, '\n'), len_keep_stash);
+	len_line = ft_strlen(stash) - len_keep_stash;
 	line = (char *) malloc(len_line * sizeof(char));
 	if (!line)
 		return (NULL);
-	line = ft_memcpy(line, tmp, len_line);
-	free(tmp);
-	tmp = (char *) malloc(len_keep_tmp * sizeof(char));
+	line = ft_memcpy(line, stash, len_line);
+	free(stash);
+	stash = (char *) malloc(len_keep_stash * sizeof(char));
 	if (!line)
 		return (NULL);
-	tmp = ft_memcpy(tmp, keep_tmp, len_keep_tmp);
-	free(keep_tmp);
+	stash = ft_memcpy(stash, keep_stash, len_keep_stash);
+	free(keep_stash);
 	return (line);
 }
 
+//Queremos que get next line cuando lo llamemos nos devuelva una frase
+
 char	*get_next_line(int fd)
 {
-    char		buff[BUFF_SIZE];
-    static char	*tmp;
+	char		buff[BUFF_SIZE];
+	static char	*stash[FD_SIZE];
 	char		*line;
 
-	tmp = NULL;
 	line = NULL;
-    if (fd <= 0)
-        return (NULL);
-	while (read(fd, buff, BUFF_SIZE) > 0 && !(fd <= 0))
-	{
-		if (tmp == NULL)
-		{
-			tmp = (char *) malloc(ft_strlen(buff) * sizeof(char));
-			if (!tmp)
-				return (NULL);
-			tmp = ft_memcpy(tmp, buff, ft_strlen(buff));
-		}
-		else
-			tmp = custom_realloc(tmp, buff);
-		if (ft_strchr(tmp, '\n') != NULL)
-			return (line = free_tmp(tmp, line));
-	}
+	if (read(fd, buff, BUFF_SIZE) < 0 && fd <= 0)
+		return (NULL);
+	if (!stash[fd])
+		stash = (char *) malloc(1 * sizeof(char));
+	if (!stash)
+		return (NULL);
+	stash = resize(stash, buff);
+	if ()
+		return (free(stash), NULL);
+	if (ft_strchr(stash, '\n') != NULL)
+		return (line = free_stash(stash, line));
 	return (NULL);
 }
 
